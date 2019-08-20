836a70228aaf41b5711dfa61e544777081c705e1
Krisztian Papp <fejlesztes@letscode.hu>
2019-08-20 13:25:48 +0200

    vuejs-22

--- vue-js/todoApp/src/config/routes.js
+++ vue-js/todoApp/src/config/routes.js
@@ -2,7 +2,7 @@ import MultiTodoList from '../components/routes/MultiTodoList.vue'
 import SingleTodoList from '../components/routes/SingleTodoList.vue'
 
 const routes = [
-    { path : '/single/:id', component: SingleTodoList},
+    { name: "single", path : '/single/:id', component: SingleTodoList},
     { path : '/', component: MultiTodoList},
 ]
 
