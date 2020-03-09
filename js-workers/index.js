5e2c30dea9ec3d7ca831320866afd7cd9c58bd52
Krisztian Papp <fejlesztes@letscode.hu>
2020-03-09 20:16:31 +0100

    js-workers-3

--- js-workers/index.js
+++ js-workers/index.js
@@ -1,25 +1,3 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/sw.js')
-        .then(function(registration) {
-            console.log("Success with scope: " + registration.scope);
-
-            document.getElementById('clear').addEventListener('click', event => {
-                navigator.serviceWorker.controller.postMessage('clear');
-            });
-
-            navigator.serviceWorker.addEventListener('message', event => {
-                if (event.data === 'deleted') {
-                    document.getElementById('message').textContent = 'Cache purged';
-                }
-            })
-
-        }, function(err) {
-            console.log("Error: " + err);
-        })
-    });
-
-    fetch('/something.json')
-    .then(response => response.json())
-    .then(r => console.log(r));
+if ('paintWorklet' in CSS) {
+    CSS.paintWorklet.addModule('paint.js');
 }
--- js-workers/worker.js
+++
@@ -1,9 +0,0 @@
-onmessage = function(e) {
-    let fib = fibonacci(e.data);
-    postMessage(fib);
-}
-
-function fibonacci(num) {
-    if (num <= 1) return 1;
-    return fibonacci(num -1) + fibonacci(num -2);
-}
