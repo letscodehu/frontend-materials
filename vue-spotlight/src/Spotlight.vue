34d5ecb7ad90a6ee27ba0c600f43f8a2e34fe52c
Krisztian Papp <fejlesztes@letscode.hu>
2020-03-21 23:03:19 +0100

    vue-spotlight-1

---
+++ vue-spotlight/src/Spotlight.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>Spotlight
+  </div>
+</template>
+
+<script>
+export default {
+    name : 'Spotlight',
+    mounted() {
+        console.log("mounted");
+    }
+}
+</script>
+
+<style>
+    body {
+        background-color: black;
+        color: #fff;
+    }
+</style>
