62a4b3be842ce8a3a24d5e5ac4f94e325dabde78
Krisztian Papp <fejlesztes@letscode.hu>
2020-03-21 23:02:33 +0100

    js-workers-3

--- js-workers/index.js
+++ js-workers/index.js
@@ -1,3 +1,30 @@
-if ('paintWorklet' in CSS) {
-    CSS.paintWorklet.addModule('paint.js');
-}
+
+    if('animationWorklet' in CSS) {
+        CSS.animationWorklet.addModule('./animator.js').then(function () {
+            
+var worklet = new WorkletAnimation(
+    'passthrough',  // name of animator
+    new KeyframeEffect(
+        document.querySelector('.box'),
+        [
+            {
+                transform: 'rotate(0)'
+            },
+            {
+                transform: 'rotate(360deg)'
+            }
+        ],
+        {  
+            duration: 2000,
+            iterations: Number.POSITIVE_INFINITY
+        }
+    ),
+    document.timeline,  // timeline
+    {}   // registerAnimator options
+);
+worklet.play();
+
+            });
+    }
+   
+     
--- js-workers/paint.js
+++
@@ -1,9 +0,0 @@
-registerPaint('letsGradient', class {
-    paint(ctx, size, properties) {
-        let gradient = ctx.createLinearGradient(size.width,size.height, 0,0);
-        gradient.addColorStop(0, "#000");
-        gradient.addColorStop(1, "#464870");
-        ctx.fillStyle = gradient;
-        ctx.fillRect(0,0, size.width, size.height);
-    }
-});
