48873c3b398d1935007328eed84642c629e86595
Papp Krisztian <fejlesztes@letscode.hu>
2021-02-18 09:36:24 +0100

    nodejs-9

--- nodejs-basics/src/api/ListEvent.js
+++ nodejs-basics/src/api/ListEvent.js
@@ -1,4 +1,23 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3308,
+    user: "root",
+    password: "pass",
+    database: "events"
+})
+connection.connect((error) => {
+    if (error) {
+        console.error("error connecting:" + error.stack)
+        return
+    }
+    console.log("connected!")
+})
 function listEvent(req, res) {
-    res.end("listEvent")
+    connection.query("SELECT * FROM events", (error, results, fields) => {
+        if (error) res.end("error!");
+        res.end(JSON.stringify(results))
+    })
 }
 module.exports = listEvent;
--- nodejs-basics/src/app.js
+++ nodejs-basics/src/app.js
@@ -1,17 +1,34 @@
 const http = require('http');
+const Router = require('router')
+const finalHandler = require('finalhandler')
+const serve = require('serve-handler')
 
+let router = Router()
 const config = require('./config/app.js');
-const router = require('./services/router.js')
 const CreateEvent = require("./api/CreateEvent.js")
-const Home = require("./api/Home.js")
 const ListEvent = require("./api/ListEvent.js")
 
-router.get("/", Home)
+const serveOptions = {
+    "public": __dirname + "/assets",
+    "rewrites": [{
+        "source": "/", "destination": "home.html"
+    }],
+    "directoryListing": false
+}
+
+
+router.get("/", (req, res) => {
+    serve(req, res, serveOptions)
+})
 router.get("/event", ListEvent)
 router.post("/event", CreateEvent)
-router.assets("/public", __dirname + "/assets")
+router.use("/public", (req, res) => {
+    return serve(req, res, serveOptions)
+})
 
-const server = http.createServer(router.handle);
+const server = http.createServer((req, res) => {
+    router(req, res, finalHandler(req, res))
+});
 
 server.listen(config.port, config.hostname, () => {
     console.log(`Server running at http://${config.hostname}:${config.port}/`);
